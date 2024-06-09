a1b0be5a304f7a9b5a25309d0975d7aa31ac4f8e
Rikudo4485 <thomespo@gmail.com>
2024-06-10 00:32:31 +0200

    add scripts :run for python & update pip

---
+++ runPython.bat
@@ -0,0 +1,22 @@
+@echo off
+
+IF "%~1"=="-help" GOTO helper
+
+SET name=main.py
+
+IF "%~1"=="-n" (
+    SET name=%~2
+)
+
+python name
+
+:helper
+echo.
+echo NOM 
+echo    runPython
+echo.
+echo SYNTAXE
+echo    runPython [OPTION]
+echo.
+echo OPTION
+echo    -n : program name which start your app [OPTIONAL] (Default:main.py).
