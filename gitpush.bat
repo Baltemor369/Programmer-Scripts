662c91c887ab7a9c9e4515f78a94e3191938b484
Baltemor369 <thomespo@gmail.com>
2024-06-11 09:29:53 +0200

    fix issue with branch

--- gitpush.bat
+++ gitpush.bat
@@ -4,24 +4,30 @@ IF "%~1"=="-help" GOTO helper
 
 SET branchName=main
 SET commitMessage=Update
+SET files=.
 
 IF NOT EXIST .gitignore GOTO errorGitignore
 
 :parse
 IF "%~1"=="" GOTO endparse
+IF "%~1"=="-f" (
+    SET files=%~2
+    SHIFT
+)
 IF "%~1"=="-b" (
-    SET "branchName=%~2"
+    SET branchName=%~2
     SHIFT
 )
 IF "%~1"=="-m" (
-    SET "commitMessage=%~2"
+    SET commitMessage=%~2
     SHIFT
 )
 SHIFT
 GOTO parse
 
 :endparse
-git add *
+git checkout %branchName%
+git add %files%
 git commit -m "%commitMessage%"
 git push -u origin %branchName%
 echo Git Command finished
@@ -41,6 +47,7 @@ echo SYNTAXE
 echo    gitpush [-OPTION] [args]
 echo.
 echo OPTION
+echo    -f : file/folder path, if multiple, put in "double-quote" [OPTIONAL] (Default:.).
 echo    -b : name of the branch [OPTIONAL] (Default:main).
 echo    -m : the commit message [OPTIONAL] (Default:Update).
 echo    -h : the helper.
